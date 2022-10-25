d605899cebc3dee4b01b8a6b89f9ce08abea2644
heyhocombo <heyhocombo@gmail.com>
2022-10-25 18:04:30 +0300

    backfinito

---
+++ iForecast/Gradient.swift
@@ -0,0 +1,41 @@
+//
+//  CAGradientLayer.swift
+//  iForecast
+//
+//  Created by Kostiantyn Kaniuka on 25.10.2022.
+//
+
+import UIKit
+
+class Gradient: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = self.bounds
+        
+    }
+    private func setUpViews() {
+        self.layer.addSublayer(gradientLayer)
+        gradientLayer.colors = [
+            UIColor.backgroundTop.cgColor,
+            UIColor.backgroundBottom.cgColor
+        ]
+    }
+}
+
+extension UIColor {
+    static let backgroundTop = UIColor(red: 0/255, green: 92/255, blue: 151/255, alpha: 1)
+    static let backgroundBottom = UIColor(red: 54/255, green: 55/255, blue: 149/255, alpha: 1)
+}
+
--- iForecast/iForeCastViewController.swift
+++ iForecast/iForeCastViewController.swift
@@ -11,7 +11,21 @@ class iForeCastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+     setUpViews()
+    }
+    
+    func setUpViews() {
+        let backgroundColorView = Gradient()
+        backgroundColorView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backgroundColorView)
+        
+        NSLayoutConstraint.activate([
+            backgroundColorView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundColorView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundColorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundColorView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
     }
 }
 
